3aff867961f190d3a3971e1fda45a4bc2aab8997
Ale Onta <casualOTN@gmail.com>
2023-07-21 21:11:59 +0200

    adds navbar css and user section

--- src/components/navbar/NavbarComponent.tsx
+++ src/components/navbar/NavbarComponent.tsx
@@ -22,28 +22,28 @@ export const NavbarComponent = () => {
       <Row className="m-0">
         <Col xs={12} className="bg-dark py-3 px-0">
           <Row className="m-0 d-flex justify-content-between">
-            <Col>
-              <img src={Logo} alt="Custom Logo Ale Onta" width={140} />
+            <Col xs={4} className="d-flex align-items-center pe-0">
+              <img src={Logo} alt="Custom Logo Ale Onta" width={90} />
             </Col>
-            <Col className="d-flex align-items-center justify-content-end">
-              <ul className="d-flex align-items-center text-light list-unstyled m-0 nav-link-list">
-                <li className="me-5 text-uppercase">
+            <Col className="d-flex align-items-center justify-content-end ms-4">
+              <ul className="d-flex align-items-center justify-content-around justify-content-md-end text-light list-unstyled m-0 nav-link-list">
+                <li className="me-md-5 text-uppercase">
                   <Link className="nav-link" to="/homepage">
                     Home
                   </Link>
                 </li>
-                <li className="me-5 text-uppercase">
+                <li className="me-md-5 text-uppercase">
                   <Link className="nav-link" to="/prospect">
                     My CV
                   </Link>
                 </li>
                 {loggedIn ? (
-                  <li className="me-5 text-uppercase">
+                  <li className="me-md-5 text-uppercase">
                     <Button className="login-btn">Login</Button>
                   </li>
                 ) : (
                   <motion.li
-                    className="me-5 text-dark position-relative logged-in-list-item"
+                    className="me-md-5 text-dark position-relative logged-in-list-item"
                     initial={false}
                     animate={isOpen ? "open" : "closed"}
                     onClick={() => setIsOpen(!isOpen)}
@@ -57,7 +57,7 @@ export const NavbarComponent = () => {
                       </span>
                     </motion.div>
                     <motion.ul
-                      className="position-absolute list-unstyled user-list py-2"
+                      className="position-absolute list-unstyled user-list bg-dark text-light pb-1"
                       variants={{
                         open: {
                           clipPath: "inset(0% 0% 0% 0% round 10px)",
@@ -80,24 +80,28 @@ export const NavbarComponent = () => {
                       }}
                       style={{ pointerEvents: isOpen ? "auto" : "none" }}
                     >
+                      <hr />
                       <motion.li
                         className="px-3 py-2 text-uppercase"
                         variants={itemVariants}
                       >
                         Address:{" "}
                       </motion.li>
+                      <hr />
                       <motion.li
                         className="px-3 py-2 text-uppercase"
                         variants={itemVariants}
                       >
                         ETH:{" "}
                       </motion.li>
+                      <hr />
                       <motion.li
                         className="px-3 py-2 text-uppercase"
                         variants={itemVariants}
                       >
                         USDC:{" "}
                       </motion.li>
+                      <hr />
                       <motion.li
                         className="px-3 py-2 text-uppercase"
                         variants={itemVariants}
